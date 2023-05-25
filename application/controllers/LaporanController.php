2e0a4a7e63df08b78753bed888181250185f5f14
Jujun Juna189 <53271616+jujunjuna189@users.noreply.github.com>
2023-05-25 23:24:46 +0700

    Update

--- application/controllers/LaporanController.php
+++ application/controllers/LaporanController.php
@@ -4,11 +4,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class LaporanController extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('PembayaranModel', 'pembayaran_model');
+    }
+
     public function index()
     {
+        $pembayaran = $this->pembayaran_model->getWithSiswa();
 
         $data['heading'] = 'Laporan';
         $data['sub_heading'] = 'Master Data';
+        $data['data'] = $pembayaran;
+        $data['controller'] = $this;
 
         $this->load->view('layouts/header'); //Header berisi link css dan font serta aset lainya
         $this->load->view('components/navbar/index'); // Navbar berisi navbar
--- application/views/laporan/index.php
+++ application/views/laporan/index.php
@@ -1,29 +1,44 @@
 <div class="mt-3">
     <div class="card rounded-4 p-3">
         <div class="card-body">
-            <div class="d-flex justify-content-end" style="gap: 10px">
-                <a href="#" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-add">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-spreadsheet" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                        <path d="M8 11h8v7h-8z"></path>
-                        <path d="M8 15h8"></path>
-                        <path d="M11 11v7"></path>
-                    </svg>
-                    Excel
-                </a>
-                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-add">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-text" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                        <path d="M9 9l1 0"></path>
-                        <path d="M9 13l6 0"></path>
-                        <path d="M9 17l6 0"></path>
-                    </svg>
-                    PDF
-                </a>
+            <div class="d-flex justify-content-between" style="gap: 10px">
+                <div class="d-flex align-items-center gap-4">
+                    <div class="form-group">
+                        <label for="Dari Tanggal">Dari Tanggal</label>
+                        <input type="date" name="from_date" id="from_date" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="Dari Tanggal">Sampai Tanggal</label>
+                        <input type="date" name="last_date" id="last_date" class="form-control">
+                    </div>
+                    <!-- <div class="form-group">
+                        <span onclick="onFilter()" class="btn btn-primary">Filter</span>
+                    </div> -->
+                </div>
+                <div>
+                    <a href="#" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modal-add">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-spreadsheet" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                            <path d="M8 11h8v7h-8z"></path>
+                            <path d="M8 15h8"></path>
+                            <path d="M11 11v7"></path>
+                        </svg>
+                        Excel
+                    </a>
+                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-add">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-text" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                            <path d="M9 9l1 0"></path>
+                            <path d="M9 13l6 0"></path>
+                            <path d="M9 17l6 0"></path>
+                        </svg>
+                        PDF
+                    </a>
+                </div>
             </div>
             <hr>
             <!-- Table -->
@@ -37,16 +52,32 @@
                             <th>Kelas</th>
                             <th>Kategori</th>
                             <th>Nominal</th>
-                            <th>Listrik</th>
-                            <th style="width: 10rem">Aksi</th>
                         </tr>
                     </thead>
                     <tbody class="fw-semibold text-dark-emphasis">
-
+                        <?php foreach ($data as $index => $val) : ?>
+                            <tr>
+                                <td><?= $index + 1 ?></td>
+                                <td><?= $val->tanggal ?></td>
+                                <td><?= $val->nama_lengkap ?></td>
+                                <td><?= $val->nama_kelas ?></td>
+                                <td><?= $val->jenis_kategori ?></td>
+                                <td><?= $controller->formatterCurrency($val->nominal) ?></td>
+                            </tr>
+                        <?php endforeach ?>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
-<!-- Modal -->
+<!-- Script -->
+<script>
+    const config = {
+        currentUrl: '<?= base_url('laporan') ?>';
+    }
+
+    const onFilter = () => {
+        console.log(config.currentUrl);
+    }
+</script>
